13c0907b487862fcc93e6cc1eeb04bd9dc93d3cd
Jenny Ye <jennyye@umich.edu>
2020-03-11 15:27:21 -0400

    Basic Vue Cli usage & todo list, implementing basic componet

---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="list-group-item text-left" :key=todo.id @click="clickButton">
+        {{ label }}
+    </button>
+</template>
+<script>
+    export default {
+        props: ['label'],
+        methods: {
+            clickButton() {
+                this.$emit('componentClick');
+            }
+        }
+    };
+</script>
